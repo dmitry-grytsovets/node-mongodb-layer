fce989db6f81d58c38ca6d602175a60d32426406
Dmitry Grytsovets <dmitry@grytsovets.com>
2011-05-26 19:31:47 +0400

    some work

--- test/test_bson.js
+++ test/test_bson.js
@@ -1,6 +1,7 @@
 exports.test=function () {
 var bson=require('bson');
-var e=bson.encode({a2:1.333,a3:11111112312312312,a1:null,hello:"world",a:1,b:{z:3,a:"привет утф8",ar:[123,234],r:/[a]/ig}});
+var e=bson.encode({bool:true,bool1:false,a2:1.333,a3:11111112312312312,a1:null,hello:"world",a:1,b:{z:3,a:"привет утф8",ar:[123,234],r:/[a]/ig},f:function(a){return a+1;},d:new Date()});
 console.dir(e);
-console.dir(bson.decode(e))
+console.dir(bson.decode(e));
+console.log(bson.decode(e).f(123));
 }
