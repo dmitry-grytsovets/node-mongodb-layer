9a5de76c42ad7e73eee50d3f1b72e5f194edd1bd
Dmitry Grytsovets <dmitry@grytsovets.com>
2011-05-26 12:00:55 +0400

    Lib and tests splitted

---
+++ tests/test_bson.js
@@ -0,0 +1,4 @@
+var bson=require('../lib/bson.js');
+var e=bson.encodeBSON({a2:1.333,a3:11111112312312312,a1:null,hello:"world",a:1,b:{z:3,a:"привет утф8",ar:[123,234],r:/[a]/ig}});
+console.dir(e);
+console.dir(bson.decodeBSON(e))
