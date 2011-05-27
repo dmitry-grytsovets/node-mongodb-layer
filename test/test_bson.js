b98385b005b188043deb860ea45453c0cc1bb6ec
Dmitry Grytsovets <dmitry@grytsovets.com>
2011-05-27 11:35:55 +0400

    start mongodb layer development

--- test/test_bson.js
+++ test/test_bson.js
@@ -1,7 +1,17 @@
 exports.test=function () {
-var bson=require('bson');
-var e=bson.encode({bool:true,bool1:false,a2:1.333,a3:11111112312312312,a1:null,hello:"world",a:1,b:{z:3,a:"привет утф8",ar:[123,234],r:/[a]/ig},f:function(a){return a+1;},d:new Date()});
-console.dir(e);
-console.dir(bson.decode(e));
-console.log(bson.decode(e).f(123));
+var bson=require('bson'),assert=require('assert');
+	
+var o={ 
+	bool:true,bool1:false,a2:1.333,a3:11111112312312312,a1:null,
+	hello:"world",a:1,b:{z:3,a:"привет утф8 あ",ar:[123,234]},
+	d:new Date()};
+var e=bson.encode(o);
+var d=bson.decode(e);
+assert.eql(o,d);
+var o1={ nan:NaN,inf:Infinity,minf:-Infinity,
+	bool:true,bool1:false,a2:1.333,a3:11111112312312312,a1:null,
+	hello:"world",a:1,b:{z:3,a:"привет утф8 あ",ar:[123,234],r:/[a]/ig},
+	f:function(a){return a+1;},d:new Date()
+	};
+assert.eql(JSON.stringify(o1),JSON.stringify(bson.decode(bson.encode(o1))));
 }
